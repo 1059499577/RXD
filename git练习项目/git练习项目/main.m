7e16674c3560e9a7177e2acfd1251ec1a84eb745
Xiangdong Ren (任向东) <renxiangdong@le.com>
2016-12-28 16:09:00 +0800

    创建空项目

---
+++ git练习项目/git练习项目/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git练习项目
+//
+//  Created by RenXiangDong on 16/12/28.
+//  Copyright © 2016年 RenXiangDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
