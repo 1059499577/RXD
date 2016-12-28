ea6ff9aa552af16678f1ecf84e3eaa2f8f46ae98
rxd1 <1059499577@qq.com>
2016-12-28 17:35:12 +0800

    1号添加Tool文件

---
+++ git练习项目/git练习项目/Tool.h
@@ -0,0 +1,13 @@
+//
+//  Tool.h
+//  git练习项目
+//
+//  Created by RenXiangDong on 16/12/28.
+//  Copyright © 2016年 RenXiangDong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tool : NSObject
+
+@end
